1c0c1bbacbab16ca8a4b318a5c9acab5b8ac81e8
newkrishna983-lang <newkrishna983@gmail.com>
2026-06-27 13:04:20 +0530

    Change BOT_TOKEN in config.py
    Updated BOT_TOKEN environment variable value.

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ import os
 
 API_ID = os.environ.get("API_ID", "34422904") #Replace With your api id
 API_HASH = os.environ.get("API_HASH", "7e0002469784f47fc08a6b3d93d7ebed") #Replace With your api hash
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "8514489418:AAGsN116TMvAda1lIUu2t547TqQ3ScaJEqg") #Replace With your bot token
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "8152006541:AAFcHBYbWr9b8CjvmIjTMtgwJGc-S00Ffg8") #Replace With your bot token
